847e0ff12a29cffb18cbd11d45c1d6f26875a1f0
Nathan Johnson <nathancj224@gmail.com>
2021-08-21 17:00:36 -0400

    Improve link.sh

--- scripts/link.sh
+++ scripts/link.sh
@@ -22,16 +22,16 @@ function link() {
   for file in $( ls -A | grep -vE "$EXCLUDE" ) ; do
     link_file="$HOME/$file"
 
-    if ! test -d "$link_file" || ! test -f "$link_file"; then
+    if test -d "$link_file" || test -f "$link_file"; then
       if [ -z "$OVERRIDE" ]; then
         echo "$link_file exists, skipping"
         continue
       fi
-      echo "Overriding $link_file"
+      echo -n "[overriding] "
     fi
 
     if [ -n "$DRYRUN" ]; then
-      echo "[dryrun] symlink $HOME/$file -> $PWD/$file"
+      echo "[dryrun symlink] $HOME/$file -> $PWD/$file"
     else
       ln -svF "$PWD/$file" "$HOME"
     fi
