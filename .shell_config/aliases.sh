c2f4dcc35655817420db4b557df2cfb6842e996f
Nathan Johnson <nathancj224@gmail.com>
2021-08-26 03:19:45 -0400

    Remove secrets.sh, move git commands to gitconfig aliases

--- .shell_config/aliases.sh
+++ .shell_config/aliases.sh
@@ -2,12 +2,6 @@ alias art="php artisan"
 
 alias phpunit="./vendor/bin/phpunit"
 
-alias git-view-ignored="git ls-files --ignored --exclude-standard"
-
-alias git-apply-ignore="git ls-files --ignored --exclude-standard | sed 's/.*/\"&\"/' | xargs git rm -r --cached"
-
-alias git-prune-branches="git branch --merged | egrep -v '(^\*|master|main|dev)' | xargs git branch -d" # deletes merged local branches
-
 # Improve ls
 export CLICOLOR=1
 export LSCOLORS=ExFxBxDxCxegedabagacad
