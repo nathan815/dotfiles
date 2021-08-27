4c0115d975ec6d19b51a38652d05d49ad7fafd4c
Nathan Johnson <nathancj224@gmail.com>
2021-08-26 21:08:41 -0400

    Add a bunch of aliases

--- .shell_config/aliases.sh
+++ .shell_config/aliases.sh
@@ -6,3 +6,23 @@ alias phpunit="./vendor/bin/phpunit"
 export CLICOLOR=1
 export LSCOLORS=ExFxBxDxCxegedabagacad
 alias ls='ls -GFh'
+
+alias c='clear'
+alias h='history'
+alias j='jobs -l'
+
+# Quick way to get out of current directory
+alias ..='cd ..'
+alias ...='cd ../../'
+alias .3='cd ../../../'
+alias .4='cd ../../../../'
+alias .5='cd ../../../../..'
+
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+
+alias path='echo -e ${PATH//:/\\n}'
+alias now='date +"%T"'
+alias nowtime=now
+alias nowdate='date +"%d-%m-%Y"'
