4773c9d02b32599a9d96a49ff15c6ad617b4c3a8
Nathan Johnson <nathancj224@gmail.com>
2021-09-29 01:57:59 -0400

    nvm redirect output and remove hardcoded node path

--- .shell_config/base.sh
+++ .shell_config/base.sh
@@ -24,17 +24,13 @@ export PATH="/usr/local/opt/ruby/bin:/usr/local/lib/ruby/gems/3.0.0/bin:$PATH"
 # Lazy load it to keep shell startup fast
 function nvm() {
 	if [ "$NVM_DIR" = "" ]; then
-		echo "loading nvm..."
+		echo "[loading nvm...]" > /dev/tty
 		export NVM_DIR="$HOME/.nvm"
 		[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 		[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 	fi
 	nvm "$@"
 }
-
-# Set our default node version here
-NODE_VERSION="v15.14.0"
-export PATH="$HOME/.nvm/versions/node/$NODE_VERSION/bin:$PATH"
 
 # heroku autocomplete setup
 HEROKU_AC_BASH_SETUP_PATH=$HOME/Library/Caches/heroku/autocomplete/bash_setup && test -f $HEROKU_AC_BASH_SETUP_PATH && source $HEROKU_AC_BASH_SETUP_PATH;
