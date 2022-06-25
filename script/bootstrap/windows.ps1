733d60c37d34df2e6cb2f036b5ca5a04d83ef708
Nathan Johnson <johnsonn@microsoft.com>
2022-06-25 03:48:51 -0400

    Add windows bootstrap and powershell + ohmyposh configs

---
+++ script/bootstrap/windows.ps1
@@ -0,0 +1,24 @@
+$dotfilesRepoPath = (Get-Item $PSScriptRoot).Parent.Parent.FullName
+
+# Install Chocolately package manager
+Set-ExecutionPolicy Bypass -Scope Process -Force;
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+Set-ExecutionPolicy RemoteSigned -Scope Process -Force;
+
+# Install Chocolately packages
+choco install -y git gh sudo cygwin make sudo docker docker-desktop azure-cli -y
+
+# PS Modules
+Set-PSRepository PSGallery -InstallationPolicy Trusted
+Install-Module PowerBash
+Install-Module posh-git
+
+# Profile
+if (! (Test-Path -Path $PROFILE)) {
+    New-Item -ItemType File -Path $PROFILE -Force
+}
+
+if (!(Get-Content $PROFILE -Raw).contains("oh-my-posh init")) {
+    Add-Content -Path $PROFILE -Value "oh-my-posh init pwsh --config $dotfilesRepoPath/oh-my-posh/amro.omp.json | Invoke-Expression"
+}
